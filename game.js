961568b2787f0a091d4b46218995dc33fdc2dc99
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-02-25 16:34:13 -0500

    Added pseudocode for gameplay overview

---
+++ game.js
@@ -0,0 +1,32 @@
+// Part One - Variable and Array Creation (upon browwser load/refresh)
+
+/** 
+ * Declare player one & two variables
+ * Declare 52 card deck, including suits & numbers
+ * Assign image to each card type 
+ * Assign value to each card: 2 through 13
+ */
+
+// Part Two - Deck Randomization & Split (upon "Start Game" button selection)
+
+/**
+ * Randomize full card deck array
+ * Split randomized card deck into two arrays: player 1 array = index 0-25, player 2 array = 26-51
+ * Assign split deck arrays to each player 
+ */
+
+// Part Three - Gameplay Functions (upon "Battle" or "War" button selection)
+
+/**
+ * Grab next index in each player card array and display
+ * Identify winner based on highest card value
+ * If values are tied, then call battle function & deal two more cards (next card in each player array). "War Total" count is updated.
+ * Identify game winner based on winner total = 52 or loser total = 0
+ */
+
+// Part Three - Wins Update & Start Again (upon winning/losing condition)
+
+/**
+ * Update game winner's total wins
+ * Allow for game to be restarted
+ */
