ae57efe823b3b38a535ce06f3de7cd5718f1f3ac
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-03-11 07:42:13 -0500

    Update to README to include game URL, screengrabs and future enhancements. Clean code for JS, CSS and HTML

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 
 # Project 1 - SEI-024 - RJ Crowder-Schaefer
 
+## Github Pages Link:
+
+* [Click to play game](https://rjcrowderschaefer.github.io/project-1-war/)
+
 ## Additional References:
 
-* [Overview & User Behaviors](https://docs.google.com/document/d/1q5OINmezczvxB7SqI4vA7HNZpvmRJ5ot9DSpHzc-dcM/edit?usp=sharing)
+* [Intial Overview & User Behaviors](https://docs.google.com/document/d/1q5OINmezczvxB7SqI4vA7HNZpvmRJ5ot9DSpHzc-dcM/edit?usp=sharing)
 
-* [Wireframe Mockups](https://www.figma.com/file/sgrSk5PUlpyOf4voi0thmm/Project-1---War---Wireframing?node-id=0%3A1&t=PREep9noM7QPLZom-1)
+* [Initial Wireframe Mockups](https://www.figma.com/file/sgrSk5PUlpyOf4voi0thmm/Project-1---War---Wireframing?node-id=0%3A1&t=PREep9noM7QPLZom-1)
 
 ## Game Overview
 
@@ -14,6 +18,8 @@
 **Num players:** 2
 
 **Objective:** Using a standard 52 card deck, two players go head to head with a winner declared when all 52 cards are in one of the player’s possession, leaving the other player without any cards.
+
+**Technologies Used:** HTML, CSS, Javascript
 
 ## Basic Logic
 
@@ -25,16 +31,57 @@
 * The winning player receives all of the losing player’s cards from the war.
 * If player runs out of cards during a war then they lose the game.
 
-## How To Play
+## Getting Started
 
 1. Decide who will be 'Player One' and who will be 'Player Two'.
-2. Select the "Start Game" button. Player cards will be displayed and a battle winner identified.
-3. Select "Let's Battle" to display another set of cards.
-4. In the scenario where two card values are equal, a "War" will be initiated and can be started by selecting "Let's War!" The card total will continue to increase until one player has a higher value card. The winner will get all cards in the pile.
+2. Select the "Start Game" button to shuffle the deck and assign 26 cards to each player.
+3. Select "Let's Battle" to display flip the top card in each player's deck. The player with the higher valued card will win the battle, indicated by the game with a message in the browser. The winning player received their card and the loser's card, which go to the bottom of the winner's deck.
+4. In the scenario where two card values are equal, a "War" will be initiated and can be started by selecting "Let's War!" The player with the higher valued card will win the war and collect all of the cards associated with the previous battle and war, placed at the bottom of their deck.
+5. If the initial war results in another tie, then two more cards will be flipped using the "Let's Battle Again" button. This is repeated until a winner is determined.
 
 ## How To Win
 
 1. A player wins once they have all 52 cards in their possession.
 2. A player will automatically lose if they run out of cards during a "War" scenario.
+
+## Game Screenshots
+
+* Start Game
+
+![View of War Game from initial page load](/img/war_start-game.png)
+
+* Ready to Battle
+
+![Button visible to initiate head-to-head battle](/img/war_lets-battle.png)
+
+* Winning player identified
+
+![Player 2 has a higher card and winner message displayed](/img/war_player2-wins.png)
+
+* Going to War
+
+![Players 1 & 2 card values are the same, time to go to war](/img/war_war-time.png)
+
+* Winning A War
+
+![Player 1 has a higher card and wins the war](/img/war-war-winner.png)
+
+
+
+
+
+## Next Steps & Future Enhancements
+
+* Style & Layout
+1. Update card styling to look & feel like a real card deck
+2. Include mobile reponsive layout across primary screen breakpoints
+3. Fix button display inconsistencies
+4. General styling updates for enhanced user experience
+
+* Functinality & Interactivity
+1. Implement player winning logic 
+2. Allow player names to be manually changed
+3. Implement card flipping animation when cards are dealt
+4. Implement card stacking animation when player wins
 
 
--- game.js
+++ game.js
@@ -1,20 +1,6 @@
-// Part One - Variable and Array Creation (upon browser load/refresh)
-/**
- * create const variables for suits & values
- * createDeck function to create const unshuffled, ordered deck of cards
- * shuffleDeck function to shuffle const deck each time called
- * split shuffled deck into player 1 & player 2 (26 each): player1 array = index 0-25, player2 array = 26-51
- * Assign split deck arrays to each player
- */
 const suits = ['♠', '♥', '♦', '♣'];
 const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 const currentWarStarters = [];
-
-// const player1Slot = document.querySelector(".player1-card-slot")
-// const player2Slot = document.querySelector(".player2-card-slot")
-// const player1DeckTotal = document.querySelector(".player1-deck")
-// const player2DeckTotal = document.querySelector(".player2-deck")
-// const winnerAlert = document.querySelector(".winner-text")
 
 let p1HandsWon = 0
 let p2HandsWon = 0
@@ -25,7 +11,6 @@ document.querySelector('.lets-war').style.visibility = "hidden";
 document.querySelector('.lets-war-again').style.visibility = "hidden";
 document.querySelector('.war-name').style.visibility = "hidden";
 document.querySelector('.war-total').style.visibility = "hidden";
-
 
 class Card {
   constructor(suit, value) {
@@ -46,7 +31,6 @@ class Card {
       return this;
     }
   }
- 
 }
 
 class Deck {
@@ -63,8 +47,6 @@ shuffle() {
 
 deal() {
     let dealCard = this.cards.shift();
-    console.log(dealCard);
-    // return this.cards.shift();
     return dealCard;
   }
 }
@@ -79,15 +61,7 @@ const deckMiddle = Math.ceil(deck.cards.length / 2);
 const player1Deck = new Deck(deck.cards.slice(0, deckMiddle));
 const player2Deck = new Deck(deck.cards.slice(deckMiddle, deck.cards.length));
 
-console.log(player1Deck);
-console.log(player2Deck);
-console.log(player1Deck.cards);
-console.log(player2Deck.cards);
-
-// Part Two - Gameplay Functions (upon "Battle" or "War" button selection)
-
 function startGame() {
-    // deck.shuffle();
     document.querySelector('.start-game').style.display = 'block'; 
     this.style.visibility = 'hidden';
     document.querySelector('.lets-battle').style.visibility = "visible";
@@ -95,22 +69,17 @@ function startGame() {
     document.querySelector('.player2-deck-total').innerText = player2Deck.cards.length;
 }
 
-
 function battleTime() {
     const player1Card = player1Deck.deal();
     const player2Card = player2Deck.deal();
-    console.log(player1Card)
-    console.log(player2Card)
     const winningCard = player1Card.compare(player2Card);
 
-    
     document.querySelector('.player1-card-value-top').innerText = player1Card.value
     document.querySelector('.player1-card-value-bottom').innerText = player1Card.value
     document.querySelector('.player1-card-suit').innerText = player1Card.suit;
     document.querySelector('.player2-card-value-top').innerText = player2Card.value
     document.querySelector('.player2-card-value-bottom').innerText = player2Card.value
     document.querySelector('.player2-card-suit').innerText = player2Card.suit;
- 
 
     if (player1Card.value === player2Card.value) {
         currentWarStarters[0] = player1Card
@@ -119,25 +88,19 @@ function battleTime() {
         document.querySelector('.lets-battle').style.display = 'block'; 
         document.querySelector('.lets-battle').style.visibility = 'hidden';
         document.querySelector('.lets-war').style.visibility = "visible";
-    }
-  
-  else if (winningCard === player1Card) {
-    document.querySelector('.text').innerText = 'Player 1 wins!';
-    p1HandsWon++;
-    player1Deck.cards.push(player1Card)
-    player1Deck.cards.push(player2Card)
-  } else if (winningCard === player2Card) {
-    document.querySelector('.text').innerText = 'Player 2 wins!';
-    p2HandsWon++;
-    player2Deck.cards.push(player1Card)
-    player2Deck.cards.push(player2Card)
-  } else {
-    console.log(`There's an error happening`);
-  } console.log(
-    player1Deck,
-    player2Deck,
-  )
-  renderView();
+    } else if (winningCard === player1Card) {
+      document.querySelector('.text').innerText = 'Player 1 wins!';
+      p1HandsWon++;
+      player1Deck.cards.push(player1Card)
+      player1Deck.cards.push(player2Card)
+    } else if (winningCard === player2Card) {
+      document.querySelector('.text').innerText = 'Player 2 wins!';
+      p2HandsWon++;
+      player2Deck.cards.push(player1Card)
+      player2Deck.cards.push(player2Card)
+    } else {
+      document.querySelector('.text').innerText = 'Error. Nobody wins!';
+    } renderView();
 }
 
 function renderView() {
@@ -147,23 +110,17 @@ function renderView() {
   document.querySelector('.player2-win-total').innerHTML = p2HandsWon;
 }
 
-
 function warTime(){
     const card1 = currentWarStarters[0]
     const card2 = currentWarStarters[1]
-    console.log('clicked');
     let player1CardCurrent = player1Deck.deal();
     let player2CardCurrent = player2Deck.deal();
-    console.log(player1CardCurrent, player2CardCurrent);
     let warCards = [];
-    console.log(document.querySelector('.lets-battle'))
-    console.log(player2Deck.cards)  
+
     while (player1CardCurrent.value === player2CardCurrent.value) {
         warCards.push(player1CardCurrent, player2CardCurrent)
         player1CardCurrent = player1Deck.deal();
         player2CardCurrent = player2Deck.deal();
-        console.log(card1, card2, player1CardCurrent, player2CardCurrent)
-        
         document.querySelector('.player1-card-value-top').innerText = player1CardCurrent.value;
         document.querySelector('.player1-card-value-bottom').innerText = player1CardCurrent.value;
         document.querySelector('.player1-card-suit').innerText = player1CardCurrent.suit;
@@ -178,12 +135,7 @@ function warTime(){
         this.style.visibility = 'hidden';
         document.querySelector('.lets-war-again').style.visibility = "visible";
         document.querySelector('.text').innerText = 'The war goes on';
-
-        
     } if (player1CardCurrent.value > player2CardCurrent.value) {
-        // logic for adding to player 1 array
-        // document.querySelector('.player1-card-num').innerText = player1CardCurrent.value + player1CardCurrent.suit;
-        // document.querySelector('.player2-card-num').innerText = player2CardCurrent.value + player2CardCurrent.suit;
         document.querySelector('.player1-card-value-top').innerText = player1CardCurrent.value;
         document.querySelector('.player1-card-value-bottom').innerText = player1CardCurrent.value;
         document.querySelector('.player1-card-suit').innerText = player1CardCurrent.suit;
@@ -194,16 +146,12 @@ function warTime(){
         document.querySelector('.player2-deck-total').innerHTML = player2Deck.cards.length;
         player1Deck.cards.push(card1, card2, player1CardCurrent, player2CardCurrent)
         player1Deck.cards = player1Deck.cards.concat(warCards)
-        console.log(card1, card2, player1CardCurrent, player2CardCurrent)
         document.querySelector('.lets-war').style.display = 'block'; 
         this.style.visibility = 'hidden';
         document.querySelector('.lets-battle').style.visibility = "visible";
-        document.querySelector('.text').innerText = 'Player 1 Win';
+        document.querySelector('.text').innerText = 'Player 1 wins the war!';
         p1HandsWon++
     } else {
-        // logic for adding to player 2 array
-        // document.querySelector('.player1-card-num').innerText = player1CardCurrent.value + player1CardCurrent.suit;
-        // document.querySelector('.player2-card-num').innerText = player2CardCurrent.value + player2CardCurrent.suit;
         document.querySelector('.player1-card-value-top').innerText = player1CardCurrent.value;
         document.querySelector('.player1-card-value-bottom').innerText = player1CardCurrent.value;
         document.querySelector('.player1-card-suit').innerText = player1CardCurrent.suit;
@@ -214,85 +162,17 @@ function warTime(){
         document.querySelector('.player2-deck-total').innerHTML = player2Deck.cards.length;
         player2Deck.cards.push(card1, card2, player1CardCurrent, player2CardCurrent)
         player2Deck.cards = player2Deck.cards.concat(warCards)
-        console.log(card1, card2, player1CardCurrent, player2CardCurrent)
-
         document.querySelector('.lets-war').style.display = 'block'; 
         this.style.visibility = 'hidden';
         document.querySelector('.lets-battle').style.visibility = "visible";
-        document.querySelector('.text').innerText = 'Player 2 Wins';
-
+        document.querySelector('.text').innerText = 'Player 2 wins the war!';
         p2HandsWon++
     }
     renderView();
     return warCards
 }
 
-
-
-/**
- * Grab & compare first index of each player deck
- * Determine winner based on highest card value
- * Move player1 & player2 cards to bottom of winner's deck
- * Update deck total numbers
- * If values are tied, then call battle function & deal two more cards (next card in each player array). "War Total" count is updated.
-
-
-
-
-// Part Three - Gameover Functions
-/**
- * Identify game winner based on winner total = 52 or loser total = 0
- * Upon game completion, update total wins for winner
- */
-
-// Part Four - DOM Manipulation
-
 document.querySelector('.start-game').addEventListener("click", startGame);
 document.querySelector('.lets-battle').addEventListener("click", battleTime);
 document.querySelector('.lets-war').addEventListener("click", warTime);
 document.querySelector('.lets-war-again').addEventListener("click", warTime);
-
-
-
-
-/**
- * 
- * Upon browser load:
- * Display 'Start Game' button
- * "Card number" and number are hidden
- * Deck total = 26
- * Total wins = 0 for each player
- * All other buttons are hidden
- *  
- * Upon 'Start Game' click:
- * Player1 & Player2 card numbers are visible and winner is indicated
- * 'Let's Battle' button appears
- * All other buttons are hidden
- * 
- * Upon card tie/war scenario:
- * Verbal indication of a tie
- * 'Let's war' button appears
- * 'Let's war, again' button appears if another tie
- * All other buttons are hidden
- * 
- * Upon game completion:
- * Winner is indicated
- * Totals wins number is updated for winner
- * 'Play again' button is displayed
- * 
- * Upon 'play again' click:
- * Player1 & Player2 card numbers are visible and winner is indicated
- * 'Let's Battle' button appears
- * All other buttons are hidden
-
-
-// Stretch features
-
-/**
- * Card design to align with look/feel of actual deck of cards
- * Overall design (background, layout, etc)
- * 'Reset button' with alert indicating game & totals will be fully reset
- */
-
-
-
